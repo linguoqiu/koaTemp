b3a66ddeccd625156d1dacd535f04130bd9973b3
Gavin <876575488@qq.com>
2023-01-03 22:34:43 +0800

    feat: update svg captcha options

--- src/api/PublicController.js
+++ src/api/PublicController.js
@@ -3,7 +3,14 @@ import svgCaptcha from 'svg-captcha';
 class PublicController {
 
   async getCaptcha (ctx) {
-    const newCaptcha = svgCaptcha.create({});
+    const newCaptcha = svgCaptcha.create({
+      size: 4,
+      ignoreChars: '0o1il',
+      color: true,
+      noise: Math.floor(Math.random() * 5),
+      width: 150,
+      height: 50,
+    });
   
     ctx.body = {
       code: 200,
